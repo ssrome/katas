c0f62ac8098b3df9114c7d1f2c46c9f455e391fc
Sabrina <sabrina.samuel@madetech.com>
2023-01-26 12:39:12 +0000

    Refactor python arithmetic to switch case

--- python/tests/test_ arithmetic.py
+++ python/tests/test_ arithmetic.py
@@ -1,28 +1,30 @@
 import pytest
 
-def arithmetic(number):
-    if number[2] == "add":
-        return number[0] + number[1]
-    elif number[2] == "subtract":
-        return number[0] - number[1]
-    elif number[2] == "multiply":
-        return number[0] * number[1]
-    elif number[2] == "divide":
-        return number[0] / number[1]
+def arithmetic(num1, num2, operator):
+    match operator:
+        case "add":
+            return num1 + num2
+        case "subtract":
+            return num1 - num2
+        case "multiply":
+            return num1 * num2
+        case "divide":
+            return num1 / num2
 
 testData = [
-    ([0, 0, "add"], 0),
-    ([0, 2, "add"], 2),
-    ([-5, 8, "add"], 3),
-    ([5, 2, "add"], 7),
-    ((5, 7, "subtract"), -2),
-    ((5, 2, "subtract"), 3),
-    ((5, -2, "subtract"), 7),
-    ((5, 2, "multiply"), 10),
-    ((8, 2, "divide"), 4),
-    ((5, 2, "divide"), 2.5)
+    (0, 0, "add", 0),
+    (0, 2, "add", 2),
+    (-5, 8, "add", 3),
+    (5, 2, "add", 7),
+    (5, 7, "subtract", -2),
+    (5, 2, "subtract", 3),
+    (5, -2, "subtract", 7),
+    (5, 2, "multiply", 10),
+    (1, 4, "multiply", 4),
+    (8, 2, "divide", 4),
+    (5, 2, "divide", 2.5)
 ]
 
-@pytest.mark.parametrize("test_input, expected", testData)
-def test_atdd_arithmetic(test_input, expected):
-  assert arithmetic(test_input) == expected
+@pytest.mark.parametrize("num1, num2, operator, expected", testData)
+def test_atdd_arithmetic(num1, num2, operator, expected):
+  assert arithmetic(num1, num2, operator) == expected
